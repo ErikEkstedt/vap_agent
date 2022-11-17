ecc49c8a890a763ae2d3c96982f67c908d655158
Erik <eeckee@gmail.com>
2022-11-17 16:05:04 +0100

    playing with zmq python->js

--- vap_agent/interface/zmq_publisher.py
+++ vap_agent/interface/zmq_publisher.py
@@ -4,12 +4,15 @@ import msgpack
 import msgpack_numpy as m
 import numpy as np
 from tqdm import tqdm
+import json
 
 m.patch()
 
+topic = "vap"
 port = 5557
 context = zmq.Context()
-INTERVAL = 0.1
+INTERVAL = 0.02
+
 
 pair = False
 if pair:
@@ -27,14 +30,28 @@ if pair:
 else:
     socket = context.socket(zmq.PUB)
     socket.bind(f"tcp://*:{port}")
-    topic = "data"
 
     i = 0
     pbar = tqdm(desc="publish data")
     while True:
-        d = {"x": np.arange(512), "y": np.random.randint(10, size=512)}
+        # d = {"x": np.arange(5), "y": np.random.randint(20, size=5)}
+        d = {
+            "x": [1, 2, 3, 4],
+            "y": [10, 11, 12, 13],
+            "p": np.random.rand(
+                1,
+            ).item(),
+        }
+        # d = b"hello world"
+        # d = bytes([1, 2, 3, 4])
+        # d = msgpack.packb(d)
+        # d = bytes(d)
+        d = json.dumps(d).encode()
+
         socket.send_string(topic, zmq.SNDMORE)
-        socket.send_pyobj(d)
+        # socket.send_pyobj(d)
+        socket.send(d)
+
         time.sleep(INTERVAL)
         i += 1
         # print(f"sent {i}")

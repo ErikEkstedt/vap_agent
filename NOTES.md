ddfd87d4dd3c06af3ea18a3836c5c6363f376bc1
Erik <eeckee@gmail.com>
2022-11-18 21:39:59 +0100

    Update visuaal and notes

--- NOTES.md
+++ NOTES.md
@@ -2,7 +2,12 @@
 
 
 * [Overbleed is recognized with context]()
+* [Overbleed at onset]()
+* [Entropy at turn-shift projections]()
 
+
+# Overview Example
+![Example](assets/session_example.png)
 
 
 # Overbleed is recognized with context
@@ -22,3 +27,35 @@ The entropy "generally" increases at complicated steps (e.g. TRP, a turn-shift i
 ![vad requires context](assets/note_3_turn_shift_projection_entropy.png)
 
 The entropy "generally" increases at the end of turns which can indicate that the model anticipates a turn-shift. During a turn the model can mostly focus on modelling the current speaker but in proximity to a turn-shift the influence of the second speaker increases which is reflected in the rise in entropy.
+
+# Add asr with some text so we can look at TurnGPT
+
+![add turngpt](assets/note_5_bc_bumps.png)
+
+We plot the prediction probability of a backchannel (zero-shot classes) from the "listener".
+
+* Common at the end of turns?
+* Common inside turns?
+    * phrase boundaries
+    * breath groups
+
+# Future and Now Projection Divergence vs Convergence
+
+![add turngpt](assets/note_6_now_future_divergence.png)
+
+
+* Divergence:
+    * The future "believes" the turn is ending (in 600-2000ms)
+    * The present indicates that the speaker will continue in the immediate future (0-600ms)
+* Convergence:
+    * *''Observation at start of conversation which feels very noisy''*
+    * The convergence indicates a clear next speaker
+* Parallel:
+    * The present and the future agrees about the current state
+
+---------------------------------------------
+
+# Add ASR with some text so we can look at TurnGPT
+
+
+![add turngpt](assets/note_4_add_turngpt.png)
--- vap_agent/visualize_turn.py
+++ vap_agent/visualize_turn.py
@@ -6,9 +6,11 @@ import matplotlib.pyplot as plt
 
 import streamlit as st
 
+import vap.functional as VF
 from vap.audio import load_waveform, log_mel_spectrogram
 from vap.utils import read_json, read_txt
 from vap.plot_utils import plot_stereo_mel_spec
+from vap_turn_taking.vap_new import VAP
 
 parser = ArgumentParser()
 parser.add_argument("--root", type=str, default="runs")
@@ -82,6 +84,16 @@ def load_vap_log(path):
     for k, v in d.items():
         d[k] = torch.tensor(v)
 
+    d["vad"] = torch.stack([d["v1"], d["v1"]], dim=-1)
+
+    # vad = (vad > 0.5).float()
+    # zero_pad = torch.zeros((100, 2))
+    # vad = torch.cat((vad, zero_pad), dim=0)
+
+    # vapper = VAP()
+    # d["labels"] = vapper.extract_labels(vad)
+    # print("d['labels']: ", tuple(d["labels"].shape))
+    # print(d["labels"])
     return d
 
 
@@ -94,13 +106,51 @@ def plot_turn_shift(vap, dialog, ax=None, lw=2):
     else:
         fig = None
 
+    # Color background based on turns
+
+    speaker_set = {"a": False, "b": False}
     for turn in dialog:
+
+        current_speaker = turn["speaker"]
         color = "b"
-        if turn["speaker"] == "b":
+        if current_speaker == "b":
             color = "orange"
-        ax.fill_betweenx(
-            y=[0, 1], x1=turn["start"], x2=turn["end"], color=color, alpha=0.1
-        )
+
+        # find times in this turn
+        after_start = turn["start"] <= vap["time"]
+        before_end = vap["time"] <= turn["end"]
+        in_idx = torch.where(torch.logical_and(after_start, before_end))
+
+        if len(in_idx[0]) > 0:
+            t = vap["time"][in_idx]
+            bc_color = "darkgreen"
+            if current_speaker == "b":
+                v = vap["p_bc_a"][in_idx]
+            else:
+                v = vap["p_bc_b"][in_idx]
+            ax.fill_between(t, v, 0, color=bc_color, alpha=0.3)
+
+        if not speaker_set[turn["speaker"]]:
+            ax.fill_betweenx(
+                y=[0, 1],
+                x1=turn["start"],
+                x2=turn["end"],
+                color=color,
+                alpha=0.1,
+                # hatch="+",
+                label="VAD " + turn["speaker"].upper(),
+            )
+            speaker_set[turn["speaker"]] = True
+        else:
+            ax.fill_betweenx(
+                y=[0, 1],
+                x1=turn["start"],
+                x2=turn["end"],
+                color=color,
+                alpha=0.1,
+                # hatch="+",
+            )
+
     ax.plot(
         vap["time"], vap["p_now"], linewidth=lw, color="r", alpha=0.6, label="P-now"
     )
@@ -120,8 +170,8 @@ def plot_turn_shift(vap, dialog, ax=None, lw=2):
         vap["p_now"],
         where=diff >= 0,
         color="b",
-        alpha=0.2,
-        label="P-diff",
+        alpha=0.1,
+        # label="A future",
     )
     ax.fill_between(
         vap["time"],
@@ -129,8 +179,8 @@ def plot_turn_shift(vap, dialog, ax=None, lw=2):
         vap["p_now"],
         where=diff <= 0,
         color="orange",
-        alpha=0.2,
-        label="P-diff",
+        alpha=0.1,
+        # label="B future",
     )
 
     ax.axhline(0.4, linewidth=2, linestyle="dotted", color="g", alpha=0.3, zorder=1)
@@ -234,21 +284,138 @@ def plot_vad(vap, ax, scale=1):
     ax[1].plot(vap["time"], vap["v2"] * scale, color="w", label="VAD")
 
 
+def plot_prosody(waveform, vap, dialog, ax, sample_rate=16000, hop_time=0.01):
+    f0 = VF.pitch_praat(
+        waveform.cpu(), sample_rate=sample_rate, hop_time=hop_time, f0_max=400
+    )
+    f0_times = torch.arange(f0.shape[-1]).float() * hop_time
+    f0[f0 == 0] = torch.nan
+    # ax.plot(
+    #     f0_times,
+    #     f0[1].log(),
+    #     "o",
+    #     markersize=4,
+    #     color="orange",
+    #     alpha=0.8,
+    #     label="B F0",
+    # )
+
+    #######################################
+    # F0
+    #######################################
+    # Plot only F0 for the current speaker
+    # F0-praat: can't handle overbleed
+    speaker_set = {"a": False, "b": False}  # only set legend once
+    for turn in dialog:
+        current_speaker = turn["speaker"]
+
+        # find times in this turn
+        after_start = turn["start"] <= f0_times
+        before_end = f0_times <= turn["end"]
+        in_idx = torch.where(torch.logical_and(after_start, before_end))
+
+        # If there are frames in this turn we plot them
+        if len(in_idx[0]) > 0:
+            t = f0_times[in_idx]
+            color = "b"
+            if current_speaker == "a":
+                f = f0[0][in_idx]
+            else:
+                f = f0[1][in_idx]
+                color = "orange"
+
+            if not speaker_set[current_speaker]:
+                ax.plot(
+                    t,
+                    f.log(),
+                    "o",
+                    markersize=4,
+                    color=color,
+                    alpha=0.8,
+                    label=current_speaker.upper() + " F0",
+                )
+                speaker_set[current_speaker] = True
+            else:
+                ax.plot(
+                    t,
+                    f.log(),
+                    "o",
+                    markersize=4,
+                    color=color,
+                    alpha=0.8,
+                )
+
+    #######################################
+    # Plot F0 envelope?
+    # env = f0.unfold(dimension=-1, step=1, size=10)
+    # env = env.max(dim=-1).values
+    # t_env = times.unfold(dimension=-1, step=1, size=10).max(dim=-1).values
+    # ax.plot(t_env, env[0], color="b", alpha=0.5)
+    # ax.plot(t_env, env[1], color="orange", alpha=0.5)
+
+    #######################################
+    # Intensity
+    #######################################
+    int_hop_time = 0.1
+    ints = VF.intensity_praat(waveform, sample_rate=sample_rate, hop_time=int_hop_time)
+    with torch.no_grad():
+        k = 15
+        c = torch.nn.Conv1d(
+            in_channels=1,
+            out_channels=1,
+            kernel_size=k,
+            stride=1,
+            bias=False,
+            padding="same",
+            padding_mode="replicate",
+        )
+        c.weight.data.fill_(1 / k)
+        ints = c(ints.unsqueeze(1))[:, 0]
+    ints_times = torch.arange(ints.shape[-1]).float() * int_hop_time
+
+    aa = ax.twinx()
+    # aa = ax
+    aa.plot(
+        ints_times,
+        ints[0].log(),
+        linestyle="dashed",
+        linewidth=3,
+        color="b",
+        alpha=0.2,
+        label="A intensity",
+    )
+    aa.plot(
+        ints_times,
+        ints[1].log(),
+        linestyle="dashed",
+        linewidth=3,
+        color="orange",
+        alpha=0.4,
+        label="B intensity",
+    )
+    aa.legend(loc="upper left")
+    aa.set_yticks([])
+    return ax
+
+
 def plot_full_global(waveform, vap, dialog):
-    fig, axs = plt.subplots(5, 1, figsize=(16, 10), sharex=True)
+    fig, axs = plt.subplots(6, 1, figsize=(16, 10), sharex=True)
 
     lw = 3
     plot_mel_spectrogram(waveform, ax=[axs[0], axs[1]])
     plot_vad(vap, scale=80, ax=[axs[0], axs[1]])
     plot_waveform(waveform, max_points=2000, ax=axs[2])
     plot_turn_shift(vap, dialog, ax=axs[3], lw=lw)
+    plot_prosody(waveform, vap, dialog, ax=axs[4])
     plot_information(vap, ax=axs[-1], lw=lw)
 
     for a in axs[:-1]:
         a.set_yticks([])
 
     for a in axs:  # not mels
-        a.legend(loc="upper left", fontsize=12)
+        # a.legend(loc="upper left", fontsize=12)
+        # a.legend(loc="lower left", fontsize=12)
+        a.legend(loc="upper right", fontsize=12)
 
     plt.subplots_adjust(
         left=0.01, bottom=None, right=0.99, top=0.99, wspace=0.01, hspace=0
